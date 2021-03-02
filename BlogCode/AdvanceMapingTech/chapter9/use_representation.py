64be738ab5ce105e1d795200e7cead87fe6082b1
hygnic <hygnic@outlook.com>
2021-03-02 14:54:26 +0800

    update chapter9

--- BlogCode/AdvanceMapingTech/chapter9/use_representation.py
+++ BlogCode/AdvanceMapingTech/chapter9/use_representation.py
@@ -16,17 +16,12 @@ mxd = arcpy.mapping.MapDocument(u"制图表达.mxd")
 df = arcpy.mapping.ListDataFrames(mxd)[0]
 target_lyr = arcpy.mapping.ListLayers(mxd)[0]
 
-# res = arcpy.Describe(target_lyr).children
-# for _r in res:
-#     print _r.name
-#     print _r.dataType
-
 # lyr样式图层
 representation_name = "repre_test2.lyr"
 representation_lyr = arcpy.mapping.Layer(representation_name)
 
 arcpy.DropRepresentation_cartography(target_lyr, "repre_test_Rep")
-s_funcname = arcpy.arcpy.AddRepresentation_cartography
+s_funcname = arcpy.AddRepresentation_cartography
 s_funcname(target_lyr,"repre_test_Rep", import_rule_layer=representation_lyr)
 
 
