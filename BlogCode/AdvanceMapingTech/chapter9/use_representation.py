49bf379f775b1ce05ebcfbf9455cbeb0ac1ceee5
hygnic <hygnic@outlook.com>
2021-03-02 12:35:20 +0800

    update chapter9

--- BlogCode/AdvanceMapingTech/chapter9/use_representation.py
+++ BlogCode/AdvanceMapingTech/chapter9/use_representation.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # ---------------------------------------------------------------------------
 # Author: LiaoChenchen
@@ -13,27 +12,22 @@ import arcpy
 import os
 print "current dir: << {} >>".format(os.getcwd())
 
-# mxd = arcpy.mapping.MapDocument("CURRENT")
 mxd = arcpy.mapping.MapDocument(u"制图表达.mxd")
 df = arcpy.mapping.ListDataFrames(mxd)[0]
-# target_lyr = arcpy.mapping.ListLayers(mxd)[0]
+target_lyr = arcpy.mapping.ListLayers(mxd)[0]
 
+# res = arcpy.Describe(target_lyr).children
+# for _r in res:
+#     print _r.name
+#     print _r.dataType
+
+# lyr样式图层
 representation_name = "repre_test2.lyr"
 representation_lyr = arcpy.mapping.Layer(representation_name)
 
-# ...
-target_lyr = arcpy.mapping.ListLayers(mxd)[0]
-res = arcpy.Describe(target_lyr).children
-for _r in res:
-    print _r.name  #① # representation_name
-    print _r.dataType #②  # RepresentationClass
-# ...
+arcpy.DropRepresentation_cartography(target_lyr, "repre_test_Rep")
+s_funcname = arcpy.arcpy.AddRepresentation_cartography
+s_funcname(target_lyr,"repre_test_Rep", import_rule_layer=representation_lyr)
 
-# # 删除制图表达后
-# arcpy.DropRepresentation_cartography(target_lyr, "representation_name")
-#
-# s_funcname = arcpy.arcpy.AddRepresentation_cartography
-# s_funcname(target_lyr,"representation_name", import_rule_layer=representation_lyr)
 
-arcpy.RefreshActiveView()
-arcpy.RefreshTOC()
+
