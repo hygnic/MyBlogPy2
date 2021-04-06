570c8052a8894e85a5e8a2896849a8d52fda731e
hygnic <hygnic@outlook.com>
2021-04-06 22:15:21 +0800

    0406

--- BlogCode/MakeFirstGISProgram/chapter6/makeGUI.py
+++ BlogCode/MakeFirstGISProgram/chapter6/makeGUI.py
@@ -3,17 +3,19 @@
 import Tkinter as tk
 import tkFileDialog
 from multiprocessing import Process
+from ttkthemes import ThemedTk #<<注释1>>
+import ttk #<<注释2>>
 import giscode
 
 
 class MyGUI(object):
     def __init__(self):
-        self.root = tk.Tk()
+        # self.root = tk.Tk()
+        self.root = ThemedTk(theme="arc") #<<注释3>>
         self.root.geometry("450x600+800+200")
         self.root.title("GIS") #设置程序名称
         self.var = tk.StringVar()
 
-        #<<注释1>>
         self.img1 = tk.PhotoImage(file="icon/shp2.gif")
         self.img2 = tk.PhotoImage(file="icon/ok2.gif")
         
@@ -23,19 +25,18 @@ class MyGUI(object):
         self.root.mainloop() # 执行循环
 
     def create_widget(self):
-        self.frame1 = tk.Frame(self.root, relief="raised", bd=3)
+        self.frame1 = ttk.Frame(self.root)
         self.frame1.pack(fill="x")
-        self.entry = tk.Entry(self.frame1)
+        self.entry = ttk.Entry(self.frame1) #<<注释4>>
         self.entry.config(textvariable=self.var)
         self.entry.pack(
             side="left",expand=True, fill="x", pady=8, padx=10
         )
-        self.but = tk.Button(
-            self.frame1, text=u"输入线要素", relief="flat", width=30
-        ) #<<注释2>>
+        self.but = tk.Button(self.frame1, relief="flat")
+        # self.but = ttk.Button(self.frame1)
         self.but.config(command=self.open_dialog)
-        self.but.config(image=self.img1) #<<注释3>>
-        self.but.pack(side="right", pady=8)
+        self.but.config(image=self.img1)
+        self.but.pack(side="right", pady=8, padx=6)
      
     def open_dialog(self):
         varrr = tkFileDialog.askopenfilename()
@@ -43,13 +44,12 @@ class MyGUI(object):
         
     def create_run_button(self):
         # 生成下方的“运行”按钮
-        self.bottom_frame = tk.Frame(self.root,relief="raised",bd=3)
+        self.bottom_frame = ttk.Frame(self.root)
         self.bottom_frame.pack(side="bottom",fill="x",anchor="s")
-        self.ok_button = tk.Button(
-            self.bottom_frame,text=u"运行", relief="flat", width=30
-        ) #<<注释4>>
-        self.ok_button.pack(side="right", pady=8)
-        self.ok_button.config(image=self.img2)#<<注释5>>
+        self.ok_button = tk.Button(self.bottom_frame, relief="flat")
+        # self.ok_button = ttk.Button(self.bottom_frame)
+        self.ok_button.pack(side="right", pady=8, padx=6)
+        self.ok_button.config(image=self.img2)
         self.ok_button.config(command=self.run_multiprocessing)
     
     # def run(self):
