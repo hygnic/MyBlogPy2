7cb23fc45ea46d14537742e13039626c6266197d
hygnic <hygnic@outlook.com>
2021-09-16 16:55:31 +0800

    update StyleTool

---
+++ BlogCode/StyleTool/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+# -------------------------------------------
+# Name:              __init__.py
+# Author:            Hygnic
+# Created on:        2021/9/16 13:29
+# Version:           
+# Reference:         
+"""
+Description:         
+Usage:               
+"""
+# -------------------------------------------
