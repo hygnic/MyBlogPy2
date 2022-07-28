d8cfd754bef68ced39bd70bcb7a40b1c08e2354b
hygnic <wdd1g@GIT@zh>
2022-07-28 13:46:25 +0800

    update styletoolbox 2.2

---
+++ BlogCode/StyleToolCode/e_s.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# -------------------------------------------
+# Name:              test_s
+# Author:            Hygnic
+# Created on:        2022/7/28 10:54
+# Version:           
+# Reference:         
+"""
+Description:         
+Usage:               
+"""
+# -------------------------------------------
+def bar(x1, x2):
+    a = x1+2
+    b = x2+3
+    return a+b
+
+bar(1,2,)
+
+# try:
+#     bar(1,2,c=3)
+#
+# except Exception as e:
+#     bar(1,2)
+#
+# print 4
